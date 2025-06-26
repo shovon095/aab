93de165a216d348c9bf9edf5fe6296855a13a1a2
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-06-26 01:28:56 -0500

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,91 +1,205 @@
-  File "tools/train.py", line 221, in main
-    return obj_cls(**args)
-    TypeErrordatasets = [build_dataset(cfg.data.train)]: __init__() missing 1 required positional argument: 'pipeline'
+# === PASTE THIS ENTIRE BLOCK INTO: work_dirs/cfinet_sodad/cfinet_r50_1x_soda.py ===
 
+_base_ = [
+    '../../configs/_base_/models/faster_rcnn_r50_fpn.py',
+    '../../configs/_base_/schedules/schedule_1x.py', 
+    '../../configs/_base_/default_runtime.py'
+]
 
-During handling of the above exception, another exception occurred:
+find_unused_parameters=True
+rpn_weight = 0.9
+model = dict(
+    type='FasterRCNN',
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=4),
+    rpn_head=dict(
+        _delete_=True,
+        type='CRPNHead',
+        num_stages=2,
+        stages=[
+            dict(
+                type='StageRefineRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                anchor_generator=dict(
+                    type='AnchorGenerator',
+                    scales=[2],
+                    ratios=[1.0],
+                    strides=[4, 8, 16, 32]),
+                refine_reg_factor=200.0,
+                refine_cfg=dict(type='dilation', dilation=3),
+                refined_feature=True,
+                sampling=False,
+                with_cls=False,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(.0, .0, .0, .0),
+                    target_stds=(0.1, 0.1, 0.5, 0.5)),
+                loss_bbox=dict(
+                    type='IoULoss', linear=True,
+                    loss_weight=10.0 * rpn_weight)),
+            dict(
+                type='StageRefineRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                refine_cfg=dict(type='offset'),
+                refined_feature=True,
+                sampling=True,
+                with_cls=True,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(.0, .0, .0, .0),
+                    target_stds=(0.05, 0.05, 0.1, 0.1)),
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=True,
+                    loss_weight=1.0 * rpn_weight),
+                loss_bbox=dict(
+                    type='IoULoss', linear=True,
+                    loss_weight=10.0 * rpn_weight))]),
+    roi_head=dict(
+        type='FIRoIHead',
+        num_gpus=1,
+        temperature=0.6,
+        contrast_loss_weights=0.50,
+        num_con_queue=256,
+        con_sampler_cfg=dict(
+            num=128,
+            pos_fraction=[0.5, 0.25, 0.125]),
+        con_queue_dir="./work_dirs/roi_feats/cfinet",
+        ins_quality_assess_cfg=dict(
+            cls_score=0.05,
+            hq_score=0.65,
+            lq_score=0.25,
+            hq_pro_counts_thr=8),
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=5,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
+    train_cfg=dict(
+        rpn=[
+            dict(
+                assigner=dict(
+                    type='DynamicAssigner',
+                    low_quality_iou_thr=0.2,
+                    base_pos_iou_thr=0.25,
+                    neg_iou_thr=0.15),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False),
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.7,
+                    min_pos_iou=0.3,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=256,
+                    pos_fraction=0.5,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=False),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False)
+        ],
+        rpn_proposal=dict(max_per_img=300, nms=dict(iou_threshold=0.8)),
+        rcnn=dict(
+            assigner=dict(
+                pos_iou_thr=0.50, neg_iou_thr=0.50, min_pos_iou=0.50),
+            sampler=dict(type='RandomSampler', num=256, pos_fraction=0.5))),
+    test_cfg=dict(
+        rpn=dict(max_per_img=300, nms=dict(iou_threshold=0.5)),
+        rcnn=dict(score_thr=0.05))
+)
 
-  File "/home/shouvon/CFINet/mmdet/datasets/builder.py", line 82, in build_dataset
-Traceback (most recent call last):
-  File "tools/train.py", line 247, in <module>
-    dataset = build_from_cfg(cfg, DATASETS, default_args)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/mmcv/utils/registry.py", line 55, in build_from_cfg
-    main()
-  File "tools/train.py", line 221, in main
-    raise type(e)(f'{obj_cls.__name__}: {e}')
-TypeError: CocoDataset: __init__() missing 1 required positional argument: 'pipeline'
-    datasets = [build_dataset(cfg.data.train)]
-  File "/home/shouvon/CFINet/mmdet/datasets/builder.py", line 82, in build_dataset
-    dataset = build_from_cfg(cfg, DATASETS, default_args)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/mmcv/utils/registry.py", line 55, in build_from_cfg
-    raise type(e)(f'{obj_cls.__name__}: {e}')
-TypeError: CocoDataset: __init__() missing 1 required positional argument: 'pipeline'
-Traceback (most recent call last):
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/mmcv/utils/registry.py", line 52, in build_from_cfg
-    return obj_cls(**args)
-TypeError: __init__() missing 1 required positional argument: 'pipeline'
+# === THE FIX IS IN THIS SECTION ===
+dataset_type = 'CocoDataset'
+data_root = '/home/shouvon/CFINet/SODA-D-1024/'
 
-During handling of the above exception, another exception occurred:
+# Define the image normalization and padding settings
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
-Traceback (most recent call last):
-  File "tools/train.py", line 247, in <module>
-    main()
-  File "tools/train.py", line 221, in main
-    datasets = [build_dataset(cfg.data.train)]
-  File "/home/shouvon/CFINet/mmdet/datasets/builder.py", line 82, in build_dataset
-    dataset = build_from_cfg(cfg, DATASETS, default_args)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/mmcv/utils/registry.py", line 55, in build_from_cfg
-    raise type(e)(f'{obj_cls.__name__}: {e}')
-TypeError: CocoDataset: __init__() missing 1 required positional argument: 'pipeline'
-ERROR:torch.distributed.elastic.multiprocessing.api:failed (exitcode: 1) local_rank: 0 (pid: 23290) of binary: /home/shouvon/miniconda3/envs/cfinet/bin/python
-Traceback (most recent call last):
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/runpy.py", line 194, in _run_module_as_main
-    return _run_code(code, main_globals, None,
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/runpy.py", line 87, in _run_code
-    exec(code, run_globals)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/launch.py", line 193, in <module>
-    main()
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/launch.py", line 189, in main
-    launch(args)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/launch.py", line 174, in launch
-    run(args)
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/run.py", line 710, in run
-    elastic_launch(
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/launcher/api.py", line 131, in __call__
-    return launch_agent(self._config, self._entrypoint, list(args))
-  File "/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/distributed/launcher/api.py", line 259, in launch_agent
-    raise ChildFailedError(
-torch.distributed.elastic.multiprocessing.errors.ChildFailedError:
-============================================================
-tools/train.py FAILED
-------------------------------------------------------------
-Failures:
-[1]:
-  time      : 2025-06-26_01:25:26
-  host      : dxs4-DGX-Station
-  rank      : 1 (local_rank: 1)
-  exitcode  : 1 (pid: 23291)
-  error_file: <N/A>
-  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
-[2]:
-  time      : 2025-06-26_01:25:26
-  host      : dxs4-DGX-Station
-  rank      : 2 (local_rank: 2)
-  exitcode  : 1 (pid: 23292)
-  error_file: <N/A>
-  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
-[3]:
-  time      : 2025-06-26_01:25:26
-  host      : dxs4-DGX-Station
-  rank      : 3 (local_rank: 3)
-  exitcode  : 1 (pid: 23293)
-  error_file: <N/A>
-  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
-------------------------------------------------------------
-Root Cause (first observed failure):
-[0]:
-  time      : 2025-06-26_01:25:26
-  host      : dxs4-DGX-Station
-  rank      : 0 (local_rank: 0)
-  exitcode  : 1 (pid: 23290)
-  error_file: <N/A>
+# Define the training pipeline
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True), # Match chip size
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+# Define the testing pipeline
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+# Construct the final data dictionary, now including the pipeline
+data = dict(
+    samples_per_gpu=1, # Kept at 1 to prevent memory issues
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'Annotations/train.json',
+        img_prefix=data_root + 'Images/train/',
+        pipeline=train_pipeline), # <<< THE MISSING PIECE IS ADDED HERE
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'Annotations/train.json',
+        img_prefix=data_root + 'Images/train/',
+        pipeline=test_pipeline), # <<< THE MISSING PIECE IS ADDED HERE
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'Annotations/train.json',
+        img_prefix=data_root + 'Images/train/',
+        pipeline=test_pipeline)) # <<< THE MISSING PIECE IS ADDED HERE
+
+# Other training settings
+fp16 = dict(loss_scale='dynamic')
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+total_epochs = 12
+evaluation = dict(interval=1000, metric='bbox')
+log_config = dict(interval=50)
