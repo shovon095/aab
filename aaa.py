1b63bffd227c150eaee243ac989fac7da7be97e3
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-06-30 01:41:49 -0500

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,62 +1,38 @@
-  warnings.warn(
-/home/shouvon/CFINet/mmdet/utils/setup_env.py:48: UserWarning: Setting MKL_NUM_THREADS environment variable for each process to be 1 in default, to avoid your system being overloaded, please further tune the variable for optimal performance in your application as needed.
-  warnings.warn(
-/home/shouvon/CFINet/mmdet/utils/setup_env.py:48: UserWarning: Setting MKL_NUM_THREADS environment variable for each process to be 1 in default, to avoid your system being overloaded, please further tune the variable for optimal performance in your application as needed.
-  warnings.warn(
-loading annotations into memory...
-loading annotations into memory...
-Done (t=0.00s)
-creating index...
-index created!
-Done (t=0.00s)
-creating index...
-index created!
-loading annotations into memory...
-Done (t=0.00s)
-creating index...
-index created!
-loading annotations into memory...
-Done (t=0.00s)
-creating index...
-index created!
-/home/shouvon/CFINet/mmdet/models/losses/iou_loss.py:266: UserWarning: DeprecationWarning: Setting "linear=True" in IOULoss is deprecated, please use "mode=`linear`" instead.
-  warnings.warn('DeprecationWarning: Setting "linear=True" in '
-/home/shouvon/CFINet/mmdet/models/losses/iou_loss.py:266: UserWarning: DeprecationWarning: Setting "linear=True" in IOULoss is deprecated, please use "mode=`linear`" instead.
-  warnings.warn('DeprecationWarning: Setting "linear=True" in '
-/home/shouvon/CFINet/mmdet/models/losses/iou_loss.py:266: UserWarning: DeprecationWarning: Setting "linear=True" in IOULoss is deprecated, please use "mode=`linear`" instead.
-  warnings.warn('DeprecationWarning: Setting "linear=True" in '
-/home/shouvon/CFINet/mmdet/models/losses/iou_loss.py:266: UserWarning: DeprecationWarning: Setting "linear=True" in IOULoss is deprecated, please use "mode=`linear`" instead.
-  warnings.warn('DeprecationWarning: Setting "linear=True" in '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:116: UserWarning: DeprecationWarning: `num_anchors` is deprecated, for consistency or also use `num_base_priors` instead
-  warnings.warn('DeprecationWarning: `num_anchors` is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:123: UserWarning: DeprecationWarning: anchor_generator is deprecated, please use "prior_generator" instead
-  warnings.warn('DeprecationWarning: anchor_generator is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:116: UserWarning: DeprecationWarning: `num_anchors` is deprecated, for consistency or also use `num_base_priors` instead
-  warnings.warn('DeprecationWarning: `num_anchors` is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:123: UserWarning: DeprecationWarning: anchor_generator is deprecated, please use "prior_generator" instead
-  warnings.warn('DeprecationWarning: anchor_generator is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:116: UserWarning: DeprecationWarning: `num_anchors` is deprecated, for consistency or also use `num_base_priors` instead
-  warnings.warn('DeprecationWarning: `num_anchors` is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:123: UserWarning: DeprecationWarning: anchor_generator is deprecated, please use "prior_generator" instead
-  warnings.warn('DeprecationWarning: anchor_generator is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:116: UserWarning: DeprecationWarning: `num_anchors` is deprecated, for consistency or also use `num_base_priors` instead
-  warnings.warn('DeprecationWarning: `num_anchors` is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:123: UserWarning: DeprecationWarning: anchor_generator is deprecated, please use "prior_generator" instead
-  warnings.warn('DeprecationWarning: anchor_generator is deprecated, '
-load checkpoint from local path: work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_10.pth
-load checkpoint from local path: work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_10.pth
-load checkpoint from local path: work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_10.pth
-load checkpoint from local path: work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_10.pth
-[                                                  ] 0/2, elapsed: 0s, ETA:/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/functional.py:445: UserWarning: torch.meshgrid: in an upcoming release, it will be required to pass the indexing argument. (Triggered internally at  /opt/conda/conda-bld/pytorch_1634272068694/work/aten/src/ATen/native/TensorShape.cpp:2157.)
-  return _VF.meshgrid(tensors, **kwargs)  # type: ignore[attr-defined]
-/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/functional.py:445: UserWarning: torch.meshgrid: in an upcoming release, it will be required to pass the indexing argument. (Triggered internally at  /opt/conda/conda-bld/pytorch_1634272068694/work/aten/src/ATen/native/TensorShape.cpp:2157.)
-  return _VF.meshgrid(tensors, **kwargs)  # type: ignore[attr-defined]
-/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/functional.py:445: UserWarning: torch.meshgrid: in an upcoming release, it will be required to pass the indexing argument. (Triggered internally at  /opt/conda/conda-bld/pytorch_1634272068694/work/aten/src/ATen/native/TensorShape.cpp:2157.)
-  return _VF.meshgrid(tensors, **kwargs)  # type: ignore[attr-defined]
-/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/functional.py:445: UserWarning: torch.meshgrid: in an upcoming release, it will be required to pass the indexing argument. (Triggered internally at  /opt/conda/conda-bld/pytorch_1634272068694/work/aten/src/ATen/native/TensorShape.cpp:2157.)
-  return _VF.meshgrid(tensors, **kwargs)  # type: ignore[attr-defined]
-[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>] 4/2, 1.4 task/s, elapsed: 3s, ETA:     0s
-Evaluating bbox...
-Loading and preparing results...
-The testing results of the whole dataset is empty.
-OrderedDict()
+    train_cfg=dict(
+        rpn=[
+            dict(
+                assigner=dict(
+                    type='DynamicAssigner',
+                    low_quality_iou_thr=0.2,
+                    base_pos_iou_thr=0.25,
+                    neg_iou_thr=0.15),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False),
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.7,
+                    min_pos_iou=0.3,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=256,
+                    pos_fraction=0.5,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=False),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False)
+        ],
+        rpn_proposal=dict(max_per_img=300, nms=dict(iou_threshold=0.8)),
+        rcnn=dict(
+            assigner=dict(
+                pos_iou_thr=0.50, neg_iou_thr=0.50, min_pos_iou=0.50),
+            sampler=dict(type='RandomSampler', num=256, pos_fraction=0.5))),
+    test_cfg=dict(
+        rpn=dict(max_per_img=300, nms=dict(iou_threshold=0.1)),
+        rcnn=dict(score_thr=0.005))
+)
+
