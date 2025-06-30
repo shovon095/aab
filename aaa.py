a64a9d49beaf66d52219d18aac11c5ad8438985a
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-06-30 01:53:19 -0500

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,2 +1,8 @@
-{"images": [{"file_name": "00065_0.jpg", "height": 1024, "width": 1024, "id": 0, "ori_id": 65, "start_coord": [0, 0]}, {"file_name": "00065_2.jpg", "height": 1024, "width": 1024, "id": 2, "ori_id": 65, "start_coord": [0, 400]}], "annotations": [], "categories": [{"id": 1, "name": "people"}, {"id": 2, "name": "rider"}, {"id": 3, "name": "bicycle"}, {"id": 4, "name": "motor"}, {"id": 5, "name": "vehicle"}, {"id": 6, "name": "traffic-sign"}, {"id": 7, "name": "traffic-light"}, {"id": 8, "name": "traffic-camera"}, {"id": 9, "name": "warning-cone"}]}
+ python create_final_subset.py
 
+Successfully loaded full annotation file: /home/shouvon/CFINet/SODA-D-1024/Annotations/test.json
+
+Creating subset with 3 images.
+Found 0 corresponding ground-truth annotations.
+
+Successfully saved the correct subset annotation file to: supervisor_test_set/annotations/mini_test.json
