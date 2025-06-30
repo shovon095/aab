49677c2592f1c5cc9d58c3b7576f71a91b58be12
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-06-29 22:04:47 -0500

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,18 +1,13 @@
- python demo/image_demo.py     demo/demo.jpg     work_dirs/cfinet_sodad/cfinet_r50_1x_soda.py     ./work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_12.pth     --device cuda   --out-file my_visualizations/result_sample.jpg
-/home/shouvon/CFINet/mmdet/models/losses/iou_loss.py:266: UserWarning: DeprecationWarning: Setting "linear=True" in IOULoss is deprecated, please use "mode=`linear`" instead.
-  warnings.warn('DeprecationWarning: Setting "linear=True" in '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:116: UserWarning: DeprecationWarning: `num_anchors` is deprecated, for consistency or also use `num_base_priors` instead
-  warnings.warn('DeprecationWarning: `num_anchors` is deprecated, '
-/home/shouvon/CFINet/mmdet/models/dense_heads/anchor_head.py:123: UserWarning: DeprecationWarning: anchor_generator is deprecated, please use "prior_generator" instead
-  warnings.warn('DeprecationWarning: anchor_generator is deprecated, '
-load checkpoint from local path: ./work_dirs/faster_rcnn_r50_fpn_cfinet_1x/epoch_12.pth
-The model and loaded state dict do not match exactly
-
-size mismatch for roi_head.bbox_head.fc_cls.weight: copying a param with shape torch.Size([10, 1024]) from checkpoint, the shape in current model is torch.Size([6, 1024]).
-size mismatch for roi_head.bbox_head.fc_cls.bias: copying a param with shape torch.Size([10]) from checkpoint, the shape in current model is torch.Size([6]).
-size mismatch for roi_head.bbox_head.fc_reg.weight: copying a param with shape torch.Size([36, 1024]) from checkpoint, the shape in current model is torch.Size([20, 1024]).
-size mismatch for roi_head.bbox_head.fc_reg.bias: copying a param with shape torch.Size([36]) from checkpoint, the shape in current model is torch.Size([20]).
-/home/shouvon/CFINet/mmdet/datasets/utils.py:66: UserWarning: "ImageToTensor" pipeline is replaced by "DefaultFormatBundle" for batch inference. It is recommended to manually replace it in the test data pipeline in your config file.
-  warnings.warn(
-/home/shouvon/miniconda3/envs/cfinet/lib/python3.8/site-packages/torch/functional.py:445: UserWarning: torch.meshgrid: in an upcoming release, it will be required to pass the indexing argument. (Triggered internally at  /opt/conda/conda-bld/pytorch_1634272068694/work/aten/src/ATen/native/TensorShape.cpp:2157.)
-  return _VF.meshgrid(tensors, **kwargs)  # type: ignore[attr-defined]
+20250625_211004.log
+20250625_211226.log
+20250625_212011.log
+20250625_212659.log
+20250625_222633.log                                                                                                                                                             20250625_223440.log
+20250625_223440.log.json
+20250625_223852.log
+20250625_223852.log.json
+20250625_224956.log
+20250626_000949.log
+cfinet_r50_1x_soda.py
+cfinet_r50_soda1024.py
+faster_rcnn_r50_fpn_cfinet_1x.py
