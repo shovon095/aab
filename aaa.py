9a6a7f3807f736aef9aeb37f0081879c0ddb1512
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-06-29 22:07:50 -0500

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,13 +1,39 @@
-20250625_211004.log
-20250625_211226.log
-20250625_212011.log
-20250625_212659.log
-20250625_222633.log                                                                                                                                                             20250625_223440.log
-20250625_223440.log.json
-20250625_223852.log
-20250625_223852.log.json
-20250625_224956.log
-20250626_000949.log
-cfinet_r50_1x_soda.py
-cfinet_r50_soda1024.py
-faster_rcnn_r50_fpn_cfinet_1x.py
+"   /home/shouvon/CFINet/work_dirs/faster_rcnn_r50_fpn_cfinet_1x
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../                                                                                                                                                                             ./
+20250626_015143.log
+20250626_015143.log.json
+20250626_102107.log
+20250626_102107.log.json
+20250626_103935.log
+20250626_103935.log.json
+20250626_105011.log
+20250626_105011.log.json
+20250626_110210.log
+20250626_110210.log.json
+20250626_110540.log
+20250626_110540.log.json
+20250626_110911.log
+20250626_110911.log.json
+20250626_112227.log
+20250626_112527.log
+20250626_125411.log
+20250626_125800.log
+20250626_125800.log.json
+20250626_130104.log
+20250626_130104.log.json
+20250626_142509.log
+20250626_142509.log.json
+20250626_143451.log
+20250626_143451.log.json
+20250626_144955.log
+20250626_144955.log.json
+20250626_155706.log
+20250626_155809.log
+20250626_155809.log.json
+epoch_1.pth
+epoch_10.pth
+"work_dirs" is a directory
